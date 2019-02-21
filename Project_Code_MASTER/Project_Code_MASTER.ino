ea332fc319765df530f4e54d48d1b1476e258566
FatemaJanahi <43363498+FatemaJanahi@users.noreply.github.com>
2019-02-21 03:44:10 -0500

    Add files via upload

---
+++ Project_Code_MASTER/Project_Code_MASTER.ino
@@ -0,0 +1,216 @@
+/* 
+ *  Filename: Project_Code_MASTER
+ *  Authors: Fatema Janahi, Yael Lissack, Jillian Brislin, Matthew Colgrove
+ *  Brief: Code for Master Arduino 
+ *  Randomly lights up lights using a random number generator and sends corresponding
+ *  integer to Slave Arduino. When time is up, buzzer sounds.
+ */
+
+//Including Libraries
+#include<Wire.h> //including the wire Library
+
+//int to hold random number
+int randomNumber; 
+
+//LED pin values
+int LED1 = 5;
+int LED2 = 2;
+int LED3 = 3;
+int LED4 = 4;
+int LED5 = 6;
+int LED6 = 7;
+int LED7 = 8;
+int LED8 = 9;
+int LED9 = 10;
+
+//piezo pin values
+int piezoPin = 12;
+
+//value of minutes before buzzer sounds
+unsigned long minutes = 60000;
+
+void setup() {
+  
+  //initiating wire library to join I2C bus 
+  Wire.begin();
+
+  //setting up leds as outputs
+  pinMode (LED1, OUTPUT);
+  pinMode (LED2, OUTPUT);
+  pinMode (LED3, OUTPUT);
+  pinMode (LED4, OUTPUT);
+  pinMode (LED5, OUTPUT);
+  pinMode (LED6, OUTPUT);
+  pinMode (LED7, OUTPUT);
+  pinMode (LED8, OUTPUT);
+  pinMode (LED9, OUTPUT);
+  
+  //seeding the random number generator
+  randomSeed(analogRead(0)); 
+}
+
+void loop() {
+  
+  //run this code for 1 min only
+  if (millis() <= minutes) { 
+    
+    randomNumber = random(1,10); //generate a random number
+    
+    Wire.beginTransmission(5); //begin transmission with Slave Arduino
+    Wire.write(randomNumber); //send the random number
+    Wire.endTransmission(); 
+
+    //Light up the lights according to the number generated
+    
+    if (randomNumber == 1)
+    {
+      //turn the first light on and all others off
+      digitalWrite (LED1, 255);
+      digitalWrite (LED2, 0);  
+      digitalWrite (LED3, 0); 
+      digitalWrite (LED4, 0); 
+      digitalWrite (LED5, 0);  
+      digitalWrite (LED6, 0); 
+      digitalWrite (LED7, 0); 
+      digitalWrite (LED8, 0);  
+      digitalWrite (LED9, 0); 
+    }
+    
+    if (randomNumber == 2)
+    {
+      //turn the second light on and all others off
+      digitalWrite (LED1, 0);
+      digitalWrite (LED2, 255);  
+      digitalWrite (LED3, 0); 
+      digitalWrite (LED4, 0); 
+      digitalWrite (LED5, 0);  
+      digitalWrite (LED6, 0); 
+      digitalWrite (LED7, 0); 
+      digitalWrite (LED8, 0);  
+      digitalWrite (LED9, 0); 
+    }
+    
+    if (randomNumber == 3)
+    {
+      //turn the third light on and all others off
+      digitalWrite (LED1, 0);
+      digitalWrite (LED2, 0);  
+      digitalWrite (LED3, 255); 
+      digitalWrite (LED4, 0); 
+      digitalWrite (LED5, 0);  
+      digitalWrite (LED6, 0); 
+      digitalWrite (LED7, 0); 
+      digitalWrite (LED8, 0);  
+      digitalWrite (LED9, 0); 
+    }
+    
+    if (randomNumber == 4)
+    {
+      //turn the fourth light on and all others off
+      digitalWrite (LED1, 0);
+      digitalWrite (LED2, 0);  
+      digitalWrite (LED3, 0); 
+      digitalWrite (LED4, 255); 
+      digitalWrite (LED5, 0);  
+      digitalWrite (LED6, 0); 
+      digitalWrite (LED7, 0); 
+      digitalWrite (LED8, 0);  
+      digitalWrite (LED9, 0); 
+    }
+    
+    if (randomNumber == 5)
+    {
+      //turn the fifth light on and all others off
+      digitalWrite (LED1, 0);
+      digitalWrite (LED2, 0);  
+      digitalWrite (LED3, 0); 
+      digitalWrite (LED4, 0); 
+      digitalWrite (LED5, 255);  
+      digitalWrite (LED6, 0); 
+      digitalWrite (LED7, 0); 
+      digitalWrite (LED8, 0);  
+      digitalWrite (LED9, 0); 
+    }
+    
+    if (randomNumber == 6)
+    {
+      //turn the sixth light on and all others off
+      digitalWrite (LED1, 0);
+      digitalWrite (LED2, 0);  
+      digitalWrite (LED3, 0); 
+      digitalWrite (LED4, 0); 
+      digitalWrite (LED5, 0);  
+      digitalWrite (LED6, 255); 
+      digitalWrite (LED7, 0); 
+      digitalWrite (LED8, 0);  
+      digitalWrite (LED9, 0); 
+    }
+    
+    if (randomNumber == 7)
+    {
+      //turn the seventh light on and all others off
+      digitalWrite (LED1, 0);
+      digitalWrite (LED2, 0);  
+      digitalWrite (LED3, 0); 
+      digitalWrite (LED4, 0); 
+      digitalWrite (LED5, 0);  
+      digitalWrite (LED6, 0); 
+      digitalWrite (LED7, 255); 
+      digitalWrite (LED8, 0);  
+      digitalWrite (LED9, 0); 
+    }
+    
+    if (randomNumber == 8)
+    {
+      //turn the eightth light on and all others off
+      digitalWrite (LED1, 0);
+      digitalWrite (LED2, 0);  
+      digitalWrite (LED3, 0); 
+      digitalWrite (LED4, 0); 
+      digitalWrite (LED5, 0);  
+      digitalWrite (LED6, 0); 
+      digitalWrite (LED7, 0); 
+      digitalWrite (LED8, 255);  
+      digitalWrite (LED9, 0); 
+    }
+    
+    if (randomNumber == 9)
+    {
+      //turn the ninth light on and all others off
+      digitalWrite (LED1, 0);
+      digitalWrite (LED2, 0);  
+      digitalWrite (LED3, 0); 
+      digitalWrite (LED4, 0); 
+      digitalWrite (LED5, 0);  
+      digitalWrite (LED6, 0); 
+      digitalWrite (LED7, 0); 
+      digitalWrite (LED8, 0);  
+      digitalWrite (LED9, 255); 
+    }
+    
+    delay(900); // keep light on for this amount of time
+    
+    //now turn all lights off to give user time to reposition him/herself
+    digitalWrite (LED1, 0);
+    digitalWrite (LED2, 0);  
+    digitalWrite (LED3, 0); 
+    digitalWrite (LED4, 0); 
+    digitalWrite (LED5, 0);  
+    digitalWrite (LED6, 0); 
+    digitalWrite (LED7, 0); 
+    digitalWrite (LED8, 0);  
+    digitalWrite (LED9, 0); 
+    
+    delay(250); // keep them off for a short time
+    } 
+    
+  // if the time is over (1 min), sound the buzzer for 2000 ms
+  if ((millis() > minutes) and ( millis() <= 62000)) { 
+      tone(piezoPin, 100, 500); // if its at one minute then make the sound 
+  }
+  else 
+  {
+     // at any other time - no sound
+  }
+ }
+ 
---
+++ Project_Code_SLAVE/Project_Code_SLAVE.ino
@@ -0,0 +1,212 @@
+/* 
+ *  Filename: Project_Code_SLAVE
+ *  Authors: Fatema Janahi, Yael Lissack, Jillian Brislin, Matthew Colgrove
+ *  Brief: Code for Slave Arduino 
+ *  Uploaded to board connected to  force sensors and LCD screen. Receives random
+ *  number from master and checks to see if corresponding force sensor is being
+ *  pressed. Adds points accordingly.
+ */
+
+//Including libraries
+#include<Wire.h>
+#include<LiquidCrystal.h>
+
+//setting up LCD screen
+const int rs = 12, en = 11, d4 =5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+//declaring variables to hold data
+int numberfrommaster;
+int points = 0;
+
+//time to run for
+unsigned long minutes = 60000;
+
+//button pin values
+const int buttonPin1 = 6;
+const int buttonPin2 = 7;
+const int buttonPin3 = 8;
+const int buttonPin4 = 9;
+const int buttonPin5 = 10;
+const int buttonPin6 = A0;
+const int buttonPin7 = A1;
+const int buttonPin8 = A2;
+const int buttonPin9 = A3;
+
+//button states set to OFF
+int buttonState1 = 0;
+int buttonState2 = 0;
+int buttonState3 = 0;
+int buttonState4 = 0;
+int buttonState5 = 0;
+int buttonState6 = 0;
+int buttonState7 = 0;
+int buttonState8 = 0;
+int buttonState9 = 0;
+
+//function receives data from Master Arduino
+void receiveEvent(int howManybytes)
+{
+    while(Wire.available())
+    {
+        numberfrommaster = Wire.read();
+    }
+}
+
+void setup() {
+    //begin serial data transmission
+    Serial.begin(9600);
+    //begin I2C Communication
+    Wire.begin(5);
+    Wire.onReceive(receiveEvent);
+    //initialize screen
+    lcd.begin(16,2);
+
+}
+
+void loop() {
+
+    //if time is not up
+    if (millis() <= minutes) {
+
+      //depending on the number received from master, read whether that
+      //button is being pressed, and increment score accordingly
+      
+      if (numberfrommaster==1)
+      {
+          delay(750); //time delay for user to press button
+          
+          buttonState1 = digitalRead(buttonPin1); //read button
+          
+          if (buttonState1 == HIGH) //increment points if pressed
+          {
+              points = points + 10;
+          }
+            delay(250);
+      }
+      
+      
+      if (numberfrommaster==2)
+      {
+          delay(750);
+          
+          buttonState2 = digitalRead(buttonPin2);
+          if (buttonState2 == HIGH)
+          {
+              points = points + 10;
+          }
+              
+            delay(250);
+      }
+
+    
+      if (numberfrommaster==3)
+      {
+          delay(750);
+          
+          buttonState3 = digitalRead(buttonPin3);
+          if (buttonState3 == HIGH)
+          {
+              points = points + 10;
+          }
+ 
+          delay(250);
+      }
+
+
+    
+      if (numberfrommaster==4)
+      {
+          delay(750);
+          
+          buttonState4 = digitalRead(buttonPin4);
+          if (buttonState4 == HIGH)
+          {
+              points = points + 10;
+            
+          }
+          delay(250);
+      }
+    
+      if (numberfrommaster==5)
+      {
+          delay(750);
+          
+          buttonState5 = digitalRead(buttonPin5);
+          if (buttonState5 == HIGH)
+          {
+              points = points + 10;
+          }
+          delay(250);
+      }
+    
+      if (numberfrommaster==6)
+      {
+          delay(750);
+          
+          buttonState6 = digitalRead(buttonPin6);
+          if (buttonState6 == HIGH)
+          {
+             points = points + 10;
+          }
+              
+          delay(250);
+      }
+              
+              
+      if (numberfrommaster==7)
+      {
+          delay(750);
+         
+          buttonState7 = digitalRead(buttonPin7);
+          if (buttonState7 == HIGH)
+          {
+            points = points + 10;
+            
+          }
+          delay(250);
+      }
+    
+    
+      if (numberfrommaster==8)
+      {
+          delay(750);
+          
+          buttonState8 = digitalRead(buttonPin8);
+          if (buttonState8 == HIGH)
+          {
+            points = points + 10;
+          }
+          delay(250);
+      }
+    
+      if (numberfrommaster==9)
+      {
+        delay(750);
+       
+        buttonState9 = digitalRead(buttonPin9);
+        if (buttonState9 == HIGH)
+        {
+          points = points + 10;
+        }
+        delay(250);
+      }
+    
+    //output points to LCD screen as game is played
+    lcd.setCursor(0, 0); 
+    lcd.print("Points:  ");
+    lcd.setCursor(0, 1);
+    lcd.print(points);
+    }
+else //time is up
+    {
+    //display total points
+    lcd.setCursor(0, 0); 
+    lcd.print("Total points:  ");
+    lcd.setCursor(0, 1);
+    lcd.print(points);
+      }
+}
+
+
+
